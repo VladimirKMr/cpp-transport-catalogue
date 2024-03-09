f11edaa59d6b61cfc8bf29c35c2084cd308bb88b
VladimirKMr <vladi@m1r1.ru>
2024-03-09 09:23:18 +0700

    +svg +json v3

--- transport-catalogue/geo.cpp
+++ transport-catalogue/geo.cpp
@@ -5,7 +5,7 @@
 
 namespace geo {
 
-    const int Earth_radius = 6371000;
+    const int EARTH_RADIUS = 6371000;
 
     double ComputeDistance(Coordinates from, Coordinates to) {
         using namespace std;
@@ -15,7 +15,7 @@ namespace geo {
         const double dr = M_PI / 180.0;
         return acos(sin(from.lat * dr) * sin(to.lat * dr)
             + cos(from.lat * dr) * cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr))
-            * Earth_radius;
+            * EARTH_RADIUS;
     }
 
 }  // namespace geo
